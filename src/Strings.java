8590b0c1acfc84bfd151eafa35b3eded3f324405
gavin <ultra.p0w3er@gmail.com>
2022-09-12 11:36:54 -0400

    monsep12

--- src/Strings.java
+++ src/Strings.java
@@ -1,5 +1,9 @@
 public class Strings {
     public static void main(String[] args){
         System.out.println("Hello");
+        String name = "Gavin";
+        System.out.println(name.substring(0,3));
+        // [ )
+
     }
 }
