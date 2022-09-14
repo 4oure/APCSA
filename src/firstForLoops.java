d035c41c2e28f502461e8ab06d5cdb5eac913781
gavin <ultra.p0w3er@gmail.com>
2022-09-14 14:43:14 -0400

    wedsep14

---
+++ src/firstForLoops.java
@@ -0,0 +1,13 @@
+public class firstForLoops {
+    public static void main(String[] args){
+        // find the sum of all digits 1 --> 27
+        int total = 0;
+
+
+        for(int x = 0; x <= 27; x++){
+         total += x;
+            
+        }
+        System.out.println(total);
+    }
+}
