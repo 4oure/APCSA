afe0571f106871de968c9750bd32fd7b97ede942
gavin <ultra.p0w3er@gmail.com>
2022-09-14 10:31:15 -0400

    wedsep14

---
+++ src/escapeCharactersAndMathClass.java
@@ -0,0 +1,11 @@
+public class escapeCharactersAndMathClass {
+    public static void main(String[] args){
+        System.out.println((int)(Math.random()*10));
+        System.out.println((int)(Math.random() * (10 - 5 + 1)) + 5); // random number between 10 and 5 inclusive
+
+        //quiz tomorrow on all this
+//
+        //
+        //
+    }
+}
