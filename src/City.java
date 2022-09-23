b7b3e43514bdf7a2dfb9345d03e074afbb62d844
gavin <ultra.p0w3er@gmail.com>
2022-09-23 11:45:36 -0400

    frisep23

---
+++ src/City.java
@@ -0,0 +1,27 @@
+public class City {
+    private String name;
+    private int population;
+    private double area;
+    private double latitude;
+    private double longitude;
+    private String mayor;
+    private int numberOfNeighborhoods;
+
+    public City(){
+
+    }
+
+    public City(String name, int population, double area, double latitude, double longitude, String mayor, int numberOfNeighborhoods){
+        this.name = name;
+        this.population = population;
+        this.area = area;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.mayor = mayor;
+        this.numberOfNeighborhoods = numberOfNeighborhoods;
+    }
+
+
+
+
+}
