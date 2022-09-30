cec3c6ac62b7244bd5f71ec106e316e0c6808335
gavin <ultra.p0w3er@gmail.com>
2022-09-30 09:46:41 -0400

    frisep30

--- src/workingWithArrays.java
+++ src/workingWithArrays.java
@@ -1,7 +1,6 @@
 public class workingWithArrays {
-    public  static void main(String[] args){
-
-        // print biggest number :))))
+    public static void main(String[] args){
+        // print biggest number in array
         int[] arr = {44,  22, 12, 7, 86, 11, 56, 99, 21, 76};
         int biggest = arr[0];
         for(int x = 0; x < arr.length - 1; x++){
@@ -9,6 +8,10 @@ public class workingWithArrays {
                 biggest  = arr[x];
             }
         }
-        System.out.println(biggest);
+
+
+        for (int x = arr.length - 1; x >= 0; x--){
+            System.out.println(arr[x]);
+        }
     }
 }
