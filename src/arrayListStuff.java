5b289182e4349dbf03eee181f99301c691fda0c8
gavin <ultra.p0w3er@gmail.com>
2022-10-05 11:39:33 -0400

    wedoct5

---
+++ src/arrayListStuff.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+public class arrayListStuff {
+    public static void main(String[] args) {
+        ArrayList<Integer> monkee = new ArrayList<Integer>();
+
+        monkee.add(4);
+        monkee.add(222);
+        monkee.add(99);
+        monkee.add(103);
+        monkee.add(20);
+        monkee.add(76);
+
+        System.out.println(monkee);
+    }
+}
