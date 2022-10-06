f2ffce74eebd8e25ee5f7a78baa51f353dd3c7ee
gavin <ultra.p0w3er@gmail.com>
2022-10-06 13:37:56 -0400

    thursoct6

--- src/createArrayListssssss.java
+++ src/createArrayListssssss.java
@@ -2,33 +2,46 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class createArrayListssssss {
-    public static void updateMulThreeToZero(ArrayList<Integer> numbers){
-        for (Integer number : numbers){
-            if(number % 3 == 0){
-               numbers.set(number-1, 0);
+    public static void updateMulThreeToZero(ArrayList<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number % 3 == 0) {
+                numbers.set(number - 1, 0);
             }
-        } System.out.println("Adjusting for any number that is divisible by three being set to zero, here is your new ArrayList: " +numbers);
+        }
+        System.out.println("Adjusting for any number that is divisible by three being set to zero, here is your new ArrayList: " + numbers);
     }
-    public static void greaterThanIndex(ArrayList<Integer> numbers){
-        for(int x = 0; x < numbers.size(); x++){
-            if(numbers.get(x) > x){
+
+    public static void greaterThanIndex(ArrayList<Integer> numbers) {
+        for (int x = 0; x < numbers.size(); x++) {
+            if (numbers.get(x) > x) {
                 numbers.set(x, 10);
             }
+        }
+        System.out.println(numbers);
+    }
+
+    public static void swapOddToEven(ArrayList<Integer> numbers) {
+        for(int x = 0; x < numbers.size(); x++){
+            if(x % 2 != 0){
+                int temp = numbers.get(x-1);
+                numbers.set(x-1, numbers.get(x));
+                numbers.set(x, temp);
+            }
+
+
 
         }
         System.out.println(numbers);
     }
-    public void swapOddtoEven(ArrayList<Integer> numbers){
-
-    }
 
     public static void main(String[] args) {
         ArrayList<Integer> hi = new ArrayList<Integer>();
-        for(int x = 1; x <= 10;x++){
+        for (int x = 1; x <= 10; x++) {
             hi.add(x);
 
         }
         System.out.println(hi);
-        greaterThanIndex(hi);
+        swapOddToEven(hi);
     }
 }
+
