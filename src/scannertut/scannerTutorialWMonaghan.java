bef3773aa0859011abd9b4bcc1a20b6dd2ac97de
gavin <ultra.p0w3er@gmail.com>
2022-11-30 17:27:09 -0500

    WedNov30

---
+++ src/scannertut/scannerTutorialWMonaghan.java
@@ -0,0 +1,32 @@
+package scannertut;
+import java.io.*;
+import java.util.*;
+
+
+public class scannerTutorialWMonaghan{
+	public static void main(String[] args) {
+		File file = new File("/Users/gavin/IdeaProjects/APCSA/src/scannertut/data");
+		double avg = 0;
+		int counter = 0;
+		try{
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()){
+
+
+				String line = scanner.nextLine();
+				String[] split = line.split(",");
+				double  newNumber = Double.parseDouble(split[1]);
+				avg+= newNumber;
+				counter++;
+
+			}
+			System.out.println("Average is " +avg/counter);
+
+		}
+		catch (FileNotFoundException e){
+			e.printStackTrace();
+
+		}
+
+	}
+}
