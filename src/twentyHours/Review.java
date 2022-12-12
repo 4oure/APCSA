85091bb3f25418deace6f4c88f99d3e2b2fd2d39
gavin <ultra.p0w3er@gmail.com>
2022-12-12 16:24:53 -0500

    MonDec12

--- src/twentyHours/Review.java
+++ src/twentyHours/Review.java
@@ -1,17 +1,13 @@
 package twentyHours;
+
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Scanner;
 import java.io.File;
-import java.util.HashMap;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Random;
-import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
-
-/**
- * Class that contains helper methods for the Review Lab
- **/
 public class Review {
 
 	private static HashMap<String, Double> sentiment = new HashMap<String, Double>();
@@ -21,44 +17,39 @@ public class Review {
 
 	private static final String SPACE = " ";
 
-	static{
+	static {
 		try {
 			Scanner input = new Scanner(new File("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/cleanSentiment.csv"));
-			while(input.hasNextLine()){
+			while (input.hasNextLine()) {
 				String[] temp = input.nextLine().split(",");
-				sentiment.put(temp[0],Double.parseDouble(temp[1]));
+				sentiment.put(temp[0], Double.parseDouble(temp[1]));
 				//System.out.println("added "+ temp[0]+", "+temp[1]);
 			}
 			input.close();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Error reading or parsing cleanSentiment.csv");
 		}
 
-
-		//read in the positive adjectives in postiveAdjectives.txt
 		try {
 			Scanner input = new Scanner(new File("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/positiveAdjectives.txt"));
-			while(input.hasNextLine()){
+			while (input.hasNextLine()) {
 				String temp = input.nextLine().trim();
 				System.out.println(temp);
 				posAdjectives.add(temp);
 			}
 			input.close();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
 		}
 
 		//read in the negative adjectives in negativeAdjectives.txt
 		try {
 			Scanner input = new Scanner(new File("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/negativeAdjectives.txt"));
-			while(input.hasNextLine()){
+			while (input.hasNextLine()) {
 				negAdjectives.add(input.nextLine().trim());
 			}
 			input.close();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Error reading or parsing negativeAdjectives.txt");
 		}
 	}
@@ -67,20 +58,18 @@ public class Review {
 	 * returns a string containing all of the text in fileName (including punctuation),
 	 * with words separated by a single space
 	 */
-	public static String textToString( String fileName )
-	{
+	public static String textToString(String fileName) {
 		String temp = "";
 		try {
 			Scanner input = new Scanner(new File(fileName));
 
 			//add 'words' in the file to the string, separated by a single space
-			while(input.hasNext()){
+			while (input.hasNext()) {
 				temp = temp + input.next() + " ";
 			}
 			input.close();
 
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Unable to locate " + fileName);
 		}
 		//make sure to remove any additional space that may have been added at the end of the string.
@@ -90,14 +79,10 @@ public class Review {
 	/**
 	 * @returns the sentiment value of word as a number between -1 (very negative) to 1 (very positive sentiment)
 	 */
-	public static double sentimentVal( String word )
-	{
-		try
-		{
+	public static double sentimentVal(String word) {
+		try {
 			return sentiment.get(word.toLowerCase());
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			return 0;
 		}
 	}
@@ -105,11 +90,10 @@ public class Review {
 	/**
 	 * Returns the ending punctuation of a string, or the empty string if there is none
 	 */
-	public static String getPunctuation(@NotNull String word )
-	{
+	public static String getPunctuation(@NotNull String word) {
 		String punc = "";
-		for(int i=word.length()-1; i >= 0; i--){
-			if(!Character.isLetterOrDigit(word.charAt(i))){
+		for (int i = word.length() - 1; i >= 0; i--) {
+			if (!Character.isLetterOrDigit(word.charAt(i))) {
 				punc = punc + word.charAt(i);
 			} else {
 				return punc;
@@ -121,15 +105,12 @@ public class Review {
 	/**
 	 * Returns the word after removing any beginning or ending punctuation
 	 */
-	public static String removePunctuation(@NotNull String word )
-	{
-		while(word.length() > 0 && !Character.isAlphabetic(word.charAt(0)))
-		{
+	public static String removePunctuation(@NotNull String word) {
+		while (word.length() > 0 && !Character.isAlphabetic(word.charAt(0))) {
 			word = word.substring(1);
 		}
-		while(word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length()-1)))
-		{
-			word = word.substring(0, word.length()-1);
+		while (word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length() - 1))) {
+			word = word.substring(0, word.length() - 1);
 		}
 
 		return word;
@@ -138,18 +119,16 @@ public class Review {
 	/**
 	 * Randomly picks a positive adjective from the positiveAdjectives.txt file and returns it.
 	 */
-	public static String randomPositiveAdj()
-	{
-		int index = (int)(Math.random() * posAdjectives.size());
+	public static String randomPositiveAdj() {
+		int index = (int) (Math.random() * posAdjectives.size());
 		return posAdjectives.get(index);
 	}
 
 	/**
 	 * Randomly picks a negative adjective from the negativeAdjectives.txt file and returns it.
 	 */
-	public static String randomNegativeAdj()
-	{
-		int index = (int)(Math.random() * negAdjectives.size());
+	public static String randomNegativeAdj() {
+		int index = (int) (Math.random() * negAdjectives.size());
 		return negAdjectives.get(index);
 
 	}
@@ -157,47 +136,104 @@ public class Review {
 	/**
 	 * Randomly picks a positive or negative adjective and returns it.
 	 */
-	public static String randomAdjective()
-	{
+	public static String randomAdjective() {
 		boolean positive = Math.random() < .5;
-		if(positive){
+		if (positive) {
 			return randomPositiveAdj();
 		} else {
 			return randomNegativeAdj();
 		}
 	}
 
-	public static double totalSentiment(String fileName){
+	public static double totalSentiment(String fileName) {
 
 		double temp = 0;
 		try {
 			Scanner input = new Scanner(new File(fileName));
 
 			//add 'words' in the file to the string, separated by a single space
-			while(input.hasNext()){
-				temp += sentimentVal(input.next());
+			while (input.hasNext()) {
+				temp += sentimentVal(removePunctuation(input.next()));
 			}
 			input.close();
 
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Unable to locate " + fileName);
 		}
 
-		return temp;}
+		return temp;
+	}
 
 
+	public static int starRating(String fileName) {
+		double number = totalSentiment(fileName);
+		if (number < -2.5) {
+			return 0;
+		} else if (number < 0) {
+			return 1;
+		} else if (number < 1) {
+			return 2;
+		} else if (number < 2) {
+			return 3;
+		} else if (number < 3) {
+			return 4;
+		} else if (number < 4) {
+			return 5;
+		}
+		return 0;
+	}
 
 
+	public static String fakeReview(String fileName, Boolean wantItGood) {
+		String last = "";
+		try {
+			Scanner input = new Scanner(new File(fileName));
+			// if temp is -1 then just conatenate
+			// if not then concatenate with randomadjective
+			while (input.hasNext()) {
+				ArrayList<String> temp = new ArrayList<>();
+				String wordToAdd = input.next().trim();
+				temp.add(wordToAdd);
 
+				//System.out.println(temp);
+				if (wantItGood) {
+					for (String x : temp) {
+						if (!x.contains("*")) {
+							last = last + " " + x;
+						} else {
+							last = last + " " + randomPositiveAdj();
+
+						}
+					}
+
+				} else {
+					for (String x : temp) {
+						if (!x.contains("*")) {
+							last = last + " " + x;
+						} else {
+							last = last + " " + randomNegativeAdj();
+						}
+
+					}
+
+				}
+
+			}
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		return last;
+
+	}
 
 
 	public static void main(String[] args) {
-		System.out.println(sentimentVal("hi"));
-		System.out.println(sentimentVal("awful"));
-		System.out.println(sentimentVal("dan"));
-		System.out.println(totalSentiment("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/sampleReview.txt"));
-
-
+//		System.out.println(sentimentVal("hi"));
+//		System.out.println(sentimentVal("awful"));
+//		System.out.println(sentimentVal("dan"));
+//		System.out.println(totalSentiment("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/sampleReview.txt"));
+//		System.out.println(starRating("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/sampleReview.txt"));
+//
+		System.out.println(fakeReview("/Users/gavin/IdeaProjects/APCSA/src/twentyHours/sampleReview.txt", false));
 	}
 }
